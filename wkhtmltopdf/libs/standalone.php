153786c13cd382bc0aed69bb7d74a86c317cb965
Florian <mrflos@gmail.com>
2014-05-05 10:31:32 +0400

    Security check for command injection (thanks @enygma ) fixes #1

--- wkhtmltopdf/libs/standalone.php
+++ wkhtmltopdf/libs/standalone.php
@@ -1,5 +1,8 @@
 <?php
 
 if (isset($_GET['sourceurl']) && isset($_GET['fullFilename']) ) {
-	exec("./wkhtmltopdf-i386 '".$_GET['sourceurl']."' ".$_GET['fullFilename']);
+	//test if sourceurl is a valid url
+	if (preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_GET['sourceurl'])) {
+		exec(escapeshellcmd("./wkhtmltopdf-i386 '".$_GET['sourceurl']."' ".$_GET['fullFilename']));
+	}
 }
