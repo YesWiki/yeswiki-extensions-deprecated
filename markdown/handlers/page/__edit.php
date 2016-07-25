af184c71d8fda643c102399b616fd895a2258725
Florian Schmitt <mrflos@gmail.com>
2016-07-25 20:00:59 +0300

    WIP markdown

--- markdown/handlers/page/__edit.php
+++ markdown/handlers/page/__edit.php
@@ -1,16 +1,20 @@
 <?php
+$this->AddCSSFile('tools/markdown/libs/vendor/font-awesome-4.5.0/css/font-awesome.min.css');
 $this->AddCSSFile('tools/markdown/libs/vendor/SimpleMDE/simplemde.min.css');
 $this->AddJavascriptFile('tools/markdown/libs/vendor/SimpleMDE/simplemde.min.js');
 $this->AddJavascript("var simplemde = new SimpleMDE({
   element: $('#SimpleMDE')[0],
   spellChecker : false,
   autofocus: true,
+  promptURLs: true,
   autosave: {
     enabled: true,
     uniqueId: 'YesWikiEdit".$this->getPageTag()."',
     delay: 1000,
   },
   parsingConfig: {
-    allowAtxHeaderWithoutSpace: true,
+    allowAtxHeaderWithoutSpace: false,
   },
+  showIcons: ['horizontal-rule'],
+  //toolbar: ['bold', 'italic', 'heading', '|', 'quote'],
 });");
